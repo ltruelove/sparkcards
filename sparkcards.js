1617add01eb88842ad900052e051da59413519fb
Levi Truelove <levi.truelove@northpointeinc.com>
2015-04-30 16:04:15 -0400

    Made the background blank and fixed the code so the letters loop.

--- sparkcards.js
+++ sparkcards.js
@@ -3,12 +3,14 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'sparkcards', { preload: prelo
 function preload() {
 
     game.load.atlasJSONHash('button', '/assets/sprites/uppercaseLetters.png', '/assets/sprites/uppercaseLetters.json');
-    game.load.image('background','/assets/misc/starfield.jpg');
+    game.load.image('background','/assets/misc/blank.png');
 
 }
 
-var button;
+var aButton;
 var background;
+var buttonIndex = 0;
+var buttonLimit = 25; //26 letters in the alphabet
 
 function create() {
 
@@ -16,7 +18,7 @@ function create() {
 
     background = game.add.tileSprite(0, 0, 800, 600, 'background');
 
-    aButton = game.add.button(game.world.centerX - 80, 220, 'button', actionOnClick, this, 0, 0, 0, 0);
+    aButton = game.add.button(game.world.centerX - 80, 220, 'button', actionOnClick, this, buttonIndex, buttonIndex, buttonIndex, buttonIndex);
 
     aButton.onInputOver.add(over, this);
     aButton.onInputOut.add(out, this);
@@ -25,19 +27,26 @@ function create() {
 }
 
 function up() {
-    console.log('button up', arguments);
+    //console.log('button up', arguments);
 }
 
 function over() {
-    console.log('button over');
+    //console.log('button over');
 }
 
 function out() {
-    console.log('button out');
+    //console.log('button out');
 }
 
 function actionOnClick () {
+    buttonIndex++;
 
-    background.visible =! background.visible;
+    if (buttonIndex > buttonLimit){
+        buttonIndex = 0;
+    }
+
+    console.log(buttonIndex);
+    aButton.setFrames(buttonIndex, buttonIndex, buttonIndex, buttonIndex);
+    //background.visible =! background.visible;
 
 }
