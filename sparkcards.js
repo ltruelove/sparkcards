07a667cb285c7c2d531a298ffd35e20564d661ac
Levi Truelove <levi.truelove@northpointeinc.com>
2015-05-01 15:25:12 -0400

    Added jquery. Playing with display.

--- sparkcards.js
+++ sparkcards.js
@@ -1,10 +1,38 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'sparkcards', { preload: preload, create: create });
+var id;
+
+$(window).resize(function() {
+    clearTimeout(id);
+    id = setTimeout(resizeGame, 300);
+});
+var height = $(window).height();
+var width = $(window).width();
+
+var game = new Phaser.Game(width, height, Phaser.AUTO, 'sparkcards', { preload: preload, create: create });
 
 function preload() {
 
     game.load.atlasJSONHash('button', '/assets/sprites/uppercaseLetters.png', '/assets/sprites/uppercaseLetters.json');
-    game.load.image('background','/assets/misc/blank.png');
+    //game.load.image('background','/assets/misc/blank.png');
+}
 
+function resizeGame() {
+    var height = $(window).height();
+    var width = $(window).width();
+        
+    game.width = width;
+    game.height = height;
+    game.stage.width = width;
+    game.stage.height = height;
+        
+    if (game.renderType === Phaser.WEBGL){
+        game.renderer.resize(width, height);
+    }
+    aButton.x = (width / 2) - 80;
+    aButton.y = (height / 2) + 80;
+
+    console.log(width);
+    console.log(height);
+    console.log(aButton.position);
 }
 
 var aButton;
@@ -16,9 +44,9 @@ function create() {
 
     game.stage.backgroundColor = '#182d3b';
 
-    background = game.add.tileSprite(0, 0, 800, 600, 'background');
+    //background = game.add.tileSprite(0, 0, 800, 600, 'background');
 
-    aButton = game.add.button(game.world.centerX - 80, 220, 'button', actionOnClick, this, buttonIndex, buttonIndex, buttonIndex, buttonIndex);
+    aButton = game.add.button(game.world.centerX - 80, game.world.centerY - 80, 'button', actionOnClick, this, buttonIndex, buttonIndex, buttonIndex, buttonIndex);
 
     aButton.onInputOver.add(over, this);
     aButton.onInputOut.add(out, this);
